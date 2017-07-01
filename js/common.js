f93e697d610fac68a25530291b40761eb4833226
Anastassia Segeda <a.segeda@uea.ac.uk>
2017-07-01 20:03:37 +0100

    Scrolling animations, responsive

--- js/common.js
+++ js/common.js
@@ -1,5 +1,52 @@
 $(document).ready(function() {
 
+
+	$("#goServices").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#services").offset().top
+    	}, "slow");
+    	return false;
+	});
+	$("#goPricing").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#pricing").offset().top
+    	}, "slow");
+    	return false;
+	});
+	$("#goTeam").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#team").offset().top
+    	}, "slow");
+    	return false;
+	});
+	$("#goClients").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#clients").offset().top
+    	}, "slow");
+    	return false;
+	});
+	$("#goContact").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#contact").offset().top
+    	}, "slow");
+    	return false;
+	});
+	$(".goup").click(function() {
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+		return false;
+	});
+
+	$(".scrolldown").click(function() {
+		$("html, body").animate({
+			scrollTop: $("#services").offset().top
+		}, "slow");
+		return false;
+	});
+
+
+
+
+
 	//Chrome Smooth Scroll
 	try {
 		$.browserSelector();
@@ -9,4 +56,10 @@ $(document).ready(function() {
 	} catch(err) {
 
 	};
+
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+
+
 });
